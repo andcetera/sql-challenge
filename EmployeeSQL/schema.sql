52482e2df69e162b24e7d1703f4cd561e14dbedf
BeckyK <barimonsterbk@gmail.com>
2023-01-10 19:18:52 -0600

    fine tune datatypes & drop order

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,9 +1,10 @@
-DROP TABLE IF EXISTS titles;
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS dept_emp;
-DROP TABLE IF EXISTS dept_manager;
+
 DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS titles;
 
 
 CREATE TABLE titles (
@@ -15,27 +16,27 @@ CREATE TABLE employees (
     emp_no INT PRIMARY KEY,
     emp_title_id VARCHAR(5) NOT NULL,
     birth_date DATE NOT NULL,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     sex VARCHAR(1) NOT NULL,
     hire_date DATE NOT NULL,
     FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
 );
 
 CREATE TABLE departments (
-    dept_no VARCHAR(5) PRIMARY KEY,
+    dept_no VARCHAR(4) PRIMARY KEY,
     dept_name VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE dept_emp (
     emp_no INT NOT NULL,
-    dept_no VARCHAR(5) NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager (
-    dept_no VARCHAR(5) NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
