2ee1819569dd936b58db495aaad606c3ba6ec9e6
BeckyK <barimonsterbk@gmail.com>
2023-01-16 15:54:35 -0600

    update schema

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -29,7 +29,7 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE dept_emp (
-    emp_no INT NOT NULL,
+    emp_no INT PRIMARY KEY,
     dept_no VARCHAR(4) NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
@@ -37,13 +37,13 @@ CREATE TABLE dept_emp (
 
 CREATE TABLE dept_manager (
     dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
+    emp_no INT PRIMARY KEY,
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE salaries (
-    emp_no INT NOT NULL,
+    emp_no INT PRIMARY KEY,
     salary INT NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
